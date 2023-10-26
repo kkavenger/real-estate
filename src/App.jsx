b668a47f2b7a12dd7c17ff30662db24f7c770cb4
kunal mishra <kunalmishra@kunals-MacBook-Air.local>
2023-10-27 00:09:25 +0530

    Adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
 
 export default function App() {
   return (
-    <div>
-      <div className="text-left text-red-500">
-        App
-      </div>
-      <h1 className="">Coding</h1>
-    </div>
     
+    <BrowserRouter>
+      <Routes>
+
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/sign-in" element = {<Signin/>}/>
+        <Route path = "/sign-up" element = {<Signup/>}/>
+        <Route path = "/about" element = {<About/>}/>
+        <Route path = "/profile" element = {<Profile/>}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
