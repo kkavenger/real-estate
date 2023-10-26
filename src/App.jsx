17b4b051edb5c016c651812d705945bbd97b7ab5
kunal mishra <kunalmishra@kunals-MacBook-Air.local>
2023-10-26 23:09:57 +0530

    directory structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+export default function App() {
+  return (
+    <div>
+      <div className="text-left text-red-500">
+        App
+      </div>
+      <h1 className="">Coding</h1>
+    </div>
+    
+  )
+}
+
+
