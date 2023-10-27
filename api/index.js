1936dc8807c2e3e3c74896bee851ab4aa9dce837
kunal mishra <kunalmishra@kunals-MacBook-Air.local>
2023-10-28 01:07:31 +0530

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => {
+    console.log("Connected to Database")
+})
+.catch((err) => {
+    console.log("Error connecting to Database");
+});
 
 app.listen(8000, () => {
     console.log("Server listening on port 8000");
