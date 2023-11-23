b50ed9da045310c01e879219475992ec1ebe185b
Kunal Mishra <avenger1.tonystark@gmail.com>
2023-11-23 21:07:38 +0530

    create update listing api routes

--- api/controllers/listing.controllers.js
+++ api/controllers/listing.controllers.js
@@ -1,4 +1,5 @@
 import listing from "../models/listing.module.js";
+import { error } from "../utilies/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -23,5 +24,25 @@ export const deleteListing = async(req, res, next) => {
     } catch (error) {
         console.log(error);
         next(error);
+    }
+}
+
+export const updateListing = async(req, res, next) => {
+    const listings = await listing.findById(req.params.id);
+    if(!listings){
+        return next(error(401, 'Listing not found'))
+    }
+    if(req.user.id !== listings.userRef){
+        return next(error(404, 'You can only update your own listings'))
+    }
+    try {
+        const updatedlistings = await listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new : true }
+        )
+        res.status(200).json(updatedlistings)
+    } catch (error) {
+        next(error)
     }
 }
